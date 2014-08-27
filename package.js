e78cd3c0d7003a3951654b2cc07a2ed83bbab591
Nathan Strauser <nathan.strauser@gmail.com>
2014-08-27 10:11:31 -0400

    0.9.0 update

--- package.js
+++ package.js
@@ -1,14 +1,17 @@
 Package.describe({
-    summary: "A banner that displays when meteor server is disconnected with countdown and reconnect option"
+    summary: "A banner that displays when meteor server is disconnected with countdown and reconnect option",
+    name: "natestrauser:connection-banner",
+    version: "0.4.3",
+    git: "https://github.com/nate-strauser/meteor-connection-banner.git"
 });
 
 Package.on_use(function (api) {
-	// CLIENT
-  api.use([
-    'deps',
-    'templating',
-    'session']
-  , 'client');
+	api.versionsFrom("METEOR@0.9.0");
+	api.use([
+	    'deps',
+	    'templating',
+	    'session']
+	  , 'client');
 
     api.add_files(['client/banner.css','client/banner.html','client/banner.js'], 'client');
 });
